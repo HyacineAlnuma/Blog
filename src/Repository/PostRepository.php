bd911c00343a290d184138007d8f546c9b0691f7
Hyacine Alnuma <hyacinealnuma@gmail.com>
2023-07-18 13:40:00 +0200

    addPost route done

--- src/Repository/PostRepository.php
+++ src/Repository/PostRepository.php
@@ -12,17 +12,17 @@ class PostRepository
     public function getPost(string $id): Post
     {
         $statement = $this->connection->getConnection()->prepare(
-            "SELECT * FROM posts WHERE id = ?"
+            "SELECT title, chapo, author, content, DATE_FORMAT(lastUpdateDate, '%d/%m/%Y à %Hh%imin%ss') AS lastUpdateDate FROM posts WHERE id = ?"
         );
         $statement->execute([$id]);
 
         $row = $statement->fetch();
         $post = new Post();
         $post->title = $row['title'];
-        $post->author = $row['author'];
-        $post->chapo = $row['chapo'];
+        $post->author = $row['chapo'];
+        $post->chapo = $row['author'];
         $post->content = $row['content'];
-        // $post->lastUpdateDate = $row['date'];
+        $post->lastUpdateDate = $row['lastUpdateDate'];
 
         return $post;
     }
@@ -30,19 +30,30 @@ class PostRepository
     public function getPosts(): array
     {
         $statement = $this->connection->getConnection()->query(
-            "SELECT * FROM posts"
+            "SELECT title, chapo, author, content, DATE_FORMAT(lastUpdateDate, '%d/%m/%Y à %Hh%imin%ss') AS lastUpdateDate FROM posts"
         );
         $posts = [];
         while(($row = $statement->fetch())) {
             $post = new Post();
             $post->title = $row['title'];
-            $post->chapo = $row['chapo'];
+            $post->author = $row['chapo'];
+            $post->chapo = $row['author'];
             $post->content = $row['content'];
-            // $post->lastUpdateDate = $row['date'];
+            $post->lastUpdateDate = $row['lastUpdateDate'];
 
             $posts[] = $post;
         }
 
         return $posts;
+    }
+
+    public function addPost($inputs)
+    {
+        date_default_timezone_set('Europe/Paris');
+        $date = date('Y-m-d H:i:s', time());
+        $statement = $this->connection->getConnection()->prepare(
+            "INSERT INTO posts(author, title, chapo, content, lastUpdateDate) VALUES ('Hyacine Alnuma', ?, ?, ?, ?)"
+        );
+        $statement->execute([$inputs['title'], $inputs['chapo'], $inputs['content'], $date]);
     }
 }
